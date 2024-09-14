0bceaac59cee78be81157d3a17f098a32bf7ed72
jewelmoradi <neginmoradi@gmail.com>
2024-09-14 23:43:11 +0800

    added the file of the second modified code

--- main.py
+++ main.py
@@ -20,8 +20,38 @@ class Book:
             print(f'the book {self.title} is not available!')
 
     def return_book(self):
-        if self.available is False:
+        if not self.available:
             self.available = True  # book is returned
             print(f'{self.title} is returned now!')
         else:
             print(f'the book {self.title} was not borrowed to be returned!')
+
+
+class Member:
+
+    def __init__(self, name: str, member_id: str):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
+
+    def __str__(self):
+        return f'Member name: {self.name} has member ID: {self.member_id}'
+
+    def borrow_book(self, book):
+        if book in self.borrowed_books:  # the member had borrowed the book before
+            print(f'the book {book.title} is already borrowed by {self.name}!')
+        elif book.available:
+            self.borrowed_books.append(book)
+        book.borrow()  # handles the case of an unavailable book too
+
+    def return_book(self, book):
+        if not book.available:  # the book is borrowed
+            if book in self.borrowed_books:  # the book is borrowed by this member
+                self.borrowed_books.remove(book)
+                book.return_book()
+            else:  # the book is not borrowed by this member
+                print(f'{self.name} has not borrowed {book.title}!')
+        else:  # the book is not borrowed
+            print(f'{book.title} was never borrowed!')
+
+
