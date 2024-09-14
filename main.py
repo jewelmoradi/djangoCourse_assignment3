217a7f6cd00d480278daf6d06817fa818837cbe5
jewelmoradi <neginmoradi@gmail.com>
2024-09-15 00:19:28 +0800

    modified the code for the third time

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ class Member:
     def __str__(self):
         return f'Member name: {self.name} has member ID: {self.member_id}'
 
-    def borrow_book(self, book):
+    def borrow_book(self, book: Book):
         if book in self.borrowed_books:  # the member had borrowed the book before
             print(f'the book {book.title} is already borrowed by {self.name}!')
         elif book.available:
             self.borrowed_books.append(book)
         book.borrow()  # handles the case of an unavailable book too
 
-    def return_book(self, book):
+    def return_book(self, book: Book):
         if not book.available:  # the book is borrowed
             if book in self.borrowed_books:  # the book is borrowed by this member
                 self.borrowed_books.remove(book)
@@ -55,3 +55,44 @@ class Member:
             print(f'{book.title} was never borrowed!')
 
 
+class Library:
+
+    def __init__(self):
+        self.books = []
+        self.members = []
+
+    def add_book(self, book: Book):
+        if book in self.books:  # book is previously added to the library
+            print(f'{book.title} is already registered in the library!')
+        else:
+            self.books.append(book)
+
+    def register_member(self, member: Member):
+        if member in self.members:  # member is previously registered in the library
+            print(f'{member.name} is already registered in the library!')
+        else:
+            self.members.append(member)
+
+    def issue_book(self, member_id, isbn):
+        for member in self.members:  # searching for member in members list
+            if member.member_id == member_id:
+                for book in self.books:  # searching for book in books list
+                    if book.isbn == isbn:
+                        member.borrow_book(book)
+                        break
+                else:
+                    print(f'there were no books with ISBN {isbn} found!')
+        else:
+            print(f'there were no members with ID {member_id} found!')
+
+    def return_book(self, member_id, isbn):
+        for member in self.members:  # searching for member in members list
+            if member.member_id == member_id:
+                for book in self.books:  # searching for book in books list
+                    if book.isbn == isbn:
+                        member.return_book(book)
+                        break
+                else:
+                    print(f'there were no books with ISBN {isbn} found!')
+        else:
+            print(f'there were no members with ID {member_id} found!')
